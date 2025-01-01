ca47aa456517b862ca5adb54ecd2417ac639917b
neiroc-02 <neiro.cabrera@yahoo.com>
2024-12-31 18:54:13 -0800

    Minor settings edits

--- event_handler/event_handler/settings.py
+++ event_handler/event_handler/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@f%wf1ps!a(bvoq2hwxjop&#+2kzq&mjsj2$(&zj035q4@r4j('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # Allow localhost 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "*"]
 
 # Application definition
 
